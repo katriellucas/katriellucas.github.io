54afc2ddadfb7f065b259e5e5e4a36c601cd1f99
Go-Merk <54755295+Go-Merk@users.noreply.github.com>
2019-09-05 19:28:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const images = ['fox1','fox2','fox3','fox4'];
-const imgElem = document.querySelector('img');
-
-function randomValueFromArray(array) {
-  let randomNo =  Math.floor(Math.random() * array.length);
-  return array[randomNo];
-}
-
-setInterval(function() {
-  let randomChoice = randomValueFromArray(images);
-  imgElem.src = 'images/' + randomChoice + '.jpg';
-}, 2000)
-
 // Register service worker to control making site work offline
 
 if('serviceWorker' in navigator) {
